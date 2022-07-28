690f8f750209979ad290616c67d5690682ae2d04
satwik <satwik@oak9.io>
2022-07-28 17:42:49 -0500

    kms

--- sm/sql.tf
+++ sm/sql.tf
@@ -1,6 +1,5 @@
 data "azurerm_client_config" "current" {}
 
-resource "aws_apigatewayv2_api" "example" {
-  name                       = "example-websocket-api"
-  protocol_type              = "WEBSOCKET"
+resource "aws_kms_key" "foo" {
+  description              = "KMS key template"
 }
