e5e2ca6159a97430478eb98672a99089bbdf2269
satwik <satwik@oak9.io>
2022-09-22 15:52:43 -0500

    update

--- aws.tf
+++ aws.tf
@@ -3,20 +3,25 @@ resource "google_storage_bucket" "a" {
   deletion_window_in_days = 10
 }
 
-resource "app_engine_flexible_app_version" "a" {
+resource "google_app_engine_flexible_app_version" "b" {
   runtime             = "python27"
+  
   handlers {
     security_level = "secure_default"
   }
   
 }
 
-resource "google_kms_crypto_key" "a" {
+resource "google_app_engine_application" "c" {
+    description = ""
+}
+
+resource "google_kms_crypto_key" "d" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
 
-resource "google_sql_database_instance" "a" {
+resource "google_sql_database_instance" "e" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 
