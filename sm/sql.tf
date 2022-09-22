0f3309a7dca7f20e86f3b33dd5b386948cc070b1
satwik <satwik@oak9.io>
2022-09-22 10:39:20 -0500

    test

--- sm/sql.tf
+++ sm/sql.tf
@@ -2,3 +2,28 @@ resource "google_storage_bucket" "a" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
+
+resource "app_engine_flexible_app_version" "a" {
+  runtime             = "python27"
+  handlers {
+    security_level = "secure_default"
+  }
+  
+}
+
+resource "google_kms_crypto_key" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+
+resource "google_sql_database_instance" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+
+  settings {
+    ip_configuration {
+      require_ssl = false
+      ipv4_enabled = true
+    }
+  }
+}
