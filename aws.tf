fe0f8a8e74bc451bb67b7075b21854896797c0f2
satwik <satwik@oak9.io>
2022-09-22 14:09:01 -0500

    gcp

--- aws.tf
+++ aws.tf
@@ -1,12 +1,29 @@
-resource "aws_fms_policy" "example" {
-  name                  = "FMS-Policy-Example"
-  exclude_resource_tags = false
+resource "google_storage_bucket" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
 }
 
-resource "aws_iot_thing" "example" {
-  name = "example"
+resource "app_engine_flexible_app_version" "a" {
+  runtime             = "python27"
+  handlers {
+    security_level = "secure_default"
+  }
+  
+}
 
-  attributes = {
-    First = "examplevalue"
+resource "google_kms_crypto_key" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+
+resource "google_sql_database_instance" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+
+  settings {
+    ip_configuration {
+      require_ssl = false
+      ipv4_enabled = true
+    }
   }
 }
