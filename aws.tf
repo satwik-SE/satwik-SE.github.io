ff32feb50f764ecc5404e3cc6c65abf48ac8667b
satwik <satwik@oak9.io>
2022-09-23 13:29:11 -0500

    container

--- aws.tf
+++ aws.tf
@@ -1,9 +1,9 @@
-resource "google_storage_bucket" "a" {
+resource "google_storage_bucket" "storage" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
 
-resource "google_app_engine_flexible_app_version" "b" {
+resource "google_app_engine_flexible_app_version" "app_version" {
   runtime             = "python27"
   
   handlers {
@@ -12,16 +12,16 @@ resource "google_app_engine_flexible_app_version" "b" {
   
 }
 
-resource "google_app_engine_application" "c" {
+resource "google_app_engine_application" "app" {
     description = ""
 }
 
-resource "google_kms_crypto_key" "d" {
+resource "google_kms_crypto_key" "kms" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
 
-resource "google_sql_database_instance" "e" {
+resource "google_sql_database_instance" "sql" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 
@@ -37,4 +37,25 @@ resource "google_dns_managed_zone" "example-zone" {
   name        = "example-zone"
   dns_name    = "example-${random_id.rnd.hex}.com."
   
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+  node_config {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    
+
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  node_config {
+    
+}
 }
