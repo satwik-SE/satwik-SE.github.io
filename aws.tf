a9f4cd2138826606393e3ce9bc4e5bfb413827b3
satwik <satwik@oak9.io>
2022-09-23 15:54:33 -0500

    added shit

--- aws.tf
+++ aws.tf
@@ -1,3 +1,44 @@
+resource "google_storage_bucket" "storage" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+
+resource "google_app_engine_flexible_app_version" "app_version" {
+  runtime             = "python27"
+  
+  handlers {
+    security_level = "secure_default"
+  }
+  
+}
+
+resource "google_app_engine_application" "app" {
+    description = ""
+}
+
+resource "google_kms_crypto_key" "kms" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+}
+
+resource "google_sql_database_instance" "sql" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+
+  settings {
+    ip_configuration {
+      require_ssl = false
+      ipv4_enabled = true
+    }
+  }
+}
+
+resource "google_dns_managed_zone" "example-zone" {
+  name        = "example-zone"
+  dns_name    = "example-${random_id.rnd.hex}.com."
+  
+}
+
 resource "google_container_cluster" "primary" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
