33689113b320fe7a808f6e540edf0c07a3d2fffa
satwik <satwik@oak9.io>
2022-09-29 01:45:50 -0500

    spanner

---
+++ gcp/spanner.tf
@@ -0,0 +1,52 @@
+resource "google_spanner_instance" "spanner" {
+  config       = "regional-europe-west1"
+  display_name = "main-instance"
+  num_nodes    = 1
+}
+
+resource "google_spanner_database" "spanner-1" {
+  instance = google_spanner_instance.spanner.name
+  name     = "my-database"
+  version_retention_period = "3d"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
+}
+
+resource "google_spanner_database" "spanner-2" {
+  instance = google_spanner_instance.spanner.name
+  name     = "my-database"
+  version_retention_period = "3d"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
+}
+
+resource "google_spanner_instance_iam_binding" "spanner"{
+    instance = google_spanner_instance.spanner.name
+    role = ""
+    members = [ "allUsers" ]
+}
+
+resource "google_spanner_instance_iam_member" "spanner"{
+    instance = google_spanner_instance.spanner.name
+    role = ""
+    member = "allAuthenticatedUsers"
+}
+
+resource "google_spanner_database_iam_binding" "spanner"{
+    instance = google_spanner_database.spanner-1.name
+    role = ""
+    members = [ "allUsers" ]
+}
+
+resource "google_spanner_database_iam_member" "spanner"{
+    instance = google_spanner_database.spanner-2.name
+    role = ""
+    member = "allAuthenticatedUsers"
+}
+
