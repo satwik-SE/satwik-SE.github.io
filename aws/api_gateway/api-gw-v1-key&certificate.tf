feab9b79b88487078c498e3be7d03e4fbf6cb355
satwik <satwik@oak9.io>
2022-12-05 03:59:27 -0600

    updated aws api

--- aws/api_gateway/api-gw-v1-key&certificate.tf
+++ aws/api_gateway/api-gw-v1-key&certificate.tf
@@ -55,3 +55,7 @@ resource "aws_api_gateway_client_certificate" "foo" {
      types = ["REGIONAL"]
    }
 }
+resource "aws_api_gateway_base_path_mapping" "foo" {
+  domain_name = aws_api_gateway_domain_name.example.domain_name
+  api_id = aws_api_gateway_rest_api.foo.id
+}
