1d316f56f350471720ddae14bfdc029fe67e5d6d
satwik <satwik@oak9.io>
2023-02-08 10:36:44 -0600

    only kube

--- aws/key.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_kms_key" "key" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-}
--- tf/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "satwikstorage"{
-    resource_group_name = "SecurityTestingRG"
-    location = "East US"
-    name = "Satwik-storag-test"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    
-}
