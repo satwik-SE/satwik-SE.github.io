ad0f7b486395b697fadec8ce3069e2e93bfcf075
satwik <satwik@oak9.io>
2023-04-25 14:51:11 -0500

    te

--- a-second-page-group/front_door/cdn-frontdoor-endpoint.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_cdn_frontdoor_endpoint" "sac_frontdoor_endpoint" {
-  name                     = "sac-cdn-frontdoor-endpoint"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.sac_testing_frontdoor_profile.id
-
-  tags = {
-    ENV = "testing"
-  }
-}
