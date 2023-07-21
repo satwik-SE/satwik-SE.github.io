798782fe9d86bf89c936babe86cb05687380e4aa
satwik <satwik@oak9.io>
2023-07-21 13:20:36 -0500

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "google_storage_bucket" "test_bucket" {
-  name          = "satwik_test"
-  location      = "US-EAST1"
-  project = var.project_id
-}
-resource "google_storage_bucket_object" "functions_file" {
-    name = "functions_object"
-    bucket = google_storage_bucket.test_bucket.name
-    content = "test_content"
+resource "aws_kms" "test_bucket" {
+  id = "test"
 }
